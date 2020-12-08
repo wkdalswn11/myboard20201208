8a7349f63caab53cc990737a950971343d41da2f
wkdalswn11 <wkdalswn133@naver.com>
2020-12-08 17:13:12 +0900

    20201208

---
+++ src/jdbc/ConnectionProvider.java
@@ -0,0 +1,34 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionProvider {
+	private static String url;
+	private static String user;
+	private static String password;
+	
+	static void setUrl(String url) {
+		ConnectionProvider.url = url;
+	}
+	static void setUser(String user) {
+		ConnectionProvider.user = user;
+	}
+	static void setPassword(String password) {
+		ConnectionProvider.password = password;
+	}
+	
+	public static Connection getConnection() {
+		Connection con = null;
+		
+		try {
+			con = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+}
